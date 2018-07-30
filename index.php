f4b1b3c33c63d80154939350edd1fcf66af254a9
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-30 16:30:02 +0200

    exo5

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+setcookie('pseudo', 'couz', time() + 365*24*3600, '/', 'partie8', false, false); //écriture cookie pseudo
+setcookie('password', 'couz', time() + 365*24*3600, '/', 'partie8', false, false); //écriture cookie mot de passe
+
+
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice 4</title>
+</head>
+<body>
+  <p>
+
+  </p>
+</body>
+</html>
